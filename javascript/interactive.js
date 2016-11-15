729c78b2842588861de754e37d1574ecbfd5eabd
Marc <phyrenight@gmail.com>
2016-11-15 05:29:16 -0500

    refact: removed some global variables

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,95 +1,101 @@
-breakTime = 5;
-workTime = 25;
-stat = 0; // off setting is zero(0) on setting is one(1)
-breakOrWork = 0;
+//var workTime = 25;
+var stat = 0; // off setting is zero(0) on setting is one(1)
+var breakOrWork = 0;
+
 function assignvar()
 {
-  document.getElementById("breaks").innerHTML = breakTime;
-  document.getElementById("work").innerHTML = workTime;
-  document.getElementById("time_left").innerHTML = workTime
+//  document.getElementById("work").innerHTML = workTime;
+//  document.getElementById("time_left").innerHTML = workTime;
 }
 
 function increaseBreak() {
-	if(breakTime < 60) {
-        breakTime++;
-	    document.getElementById("breaks").innerHTML = breakTime;
-    }
-    else{
-		document.getElementById("breaks").innerHTML = breakTime;
-	}
+  var breakTime = parseInt(document.getElementById("breaks").innerHTML);
+  if(breakTime < 60) {
+    breakTime++;
+    document.getElementById("breaks").innerHTML = breakTime;
+  }
+  else{
+	document.getElementById("breaks").innerHTML = breakTime;
+  }
 }
 
 function decreaseBreak() {
-	if(breakTime > 0) {
-        breakTime--;
-	    document.getElementById("breaks").innerHTML = breakTime;
-    }
-	else {
-		document.getElementById("breaks").innerHTML = breakTime;
-	}
+  var breakTime = parseInt(document.getElementById("breaks").innerHTML);
+  if(breakTime > 0) {
+    breakTime--;
+    document.getElementById("breaks").innerHTML = breakTime;
+  }
+  else {
+	document.getElementById("breaks").innerHTML = breakTime;
+  }
 }
 
 function increaseWork() {
-    if( workTime < 60){
-	    workTime++;
-	    document.getElementById("work").innerHTML = workTime;
-    }
-	else{
-		document.getElementById("work").innerHTML = workTime;
-	}
+  var workTime = parseInt(document.getElementById("work").innerHTML);
+  if( workTime < 60){
+    workTime++;
+    document.getElementById("work").innerHTML = workTime;
+  }
+  else{
+	document.getElementById("work").innerHTML = workTime;
+  }
 }
+
 function decreaseWork() {
-	if(workTime > 0){
-        workTime--;
-	    document.getElementById("work").innerHTML = workTime;
-    }
-	else {
-		document.getElementById("work").innerHTML = workTime;
-	}
+  var workTime = parseInt(document.getElementById("work").innerHTML);
+  if(workTime > 0){
+    workTime--;
+    document.getElementById("work").innerHTML = workTime;
+  }
+  else {
+    document.getElementById("work").innerHTML = workTime;
+  }
 }
+
 function clock(){
-	alarmAndSwitch();
-	var timerTime = document.getElementById("time_left").innerHTML;
-	timerTime = timerTime + ":00"
-	var ss = timerTime.split(":");
-	var d = new Date();
-	d.setHours(0);
-	d.setMinutes(ss[0]);
-	d.setSeconds(ss[1]);
-	var d2 = new Date(d.valueOf() - 1000);
-	tempDate = d2.toTimeString().split(" ");
-	tD = tempDate[0].split(":");
-	document.getElementById("time_left").innerHTML = (tD[1]+ ":" +tD[2]);
+  alarmAndSwitch();
+  var timerTime = document.getElementById("time_left").innerHTML;
+  timerTime = timerTime + ":00";
+  var ss = timerTime.split(":");
+  var d = new Date();
+  d.setHours(0);
+  d.setMinutes(ss[0]);
+  d.setSeconds(ss[1]);
+  var d2 = new Date(d.valueOf() - 1000);
+  var tempDate = d2.toTimeString().split(" ");
+  var tD = tempDate[0].split(":");
+  document.getElementById("time_left").innerHTML = (tD[1]+ ":" +tD[2]);
 } 
 
 function alarmAndSwitch() {
-    timer = document.getElementById("time_left").innerHTML;
-    if(timer < "00:01"){
-           soundAlarm()
-        if(breakOrWork < 1){
-            breakOrWork = 1
-            document.getElementById("time_left").innerHTML = breakTime;
-	    }
-        else{
-            breakOrWork = 0;
-            document.getElementById("time_left").innerHTML = workTime;
-        }
+  var timer = document.getElementById("time_left").innerHTML;
+  if(timer < "00:01"){
+    soundAlarm();
+    if(breakOrWork < 1){
+      breakOrWork = 1;
+      document.getElementById("time_left").innerHTML = breakTime;
     }
+    else{
+      breakOrWork = 0;
+      document.getElementById("time_left").innerHTML = workTime;
+    }
+  }
 }
+
 function startStop(){
-	if (stat == 1){ // stops the clock
-		clearInterval(tickTock);
-		stat = 0;
-		document.getElementById("time_left").innerHTML = workTime;
-	}
-	else { // starts the clock
-		stat = 1;
-		document.getElementById("time_left").innerHTML = workTime;
-		tickTock = setInterval(clock,1000);
-	}
+  if (stat == 1){ // stops the clock
+	clearInterval(tickTock);
+	stat = 0;
+	document.getElementById("time_left").innerHTML = workTime;
+  }
+  else { // starts the clock
+	stat = 1;
+	document.getElementById("time_left").innerHTML = workTime;
+	tickTock = setInterval(clock,1000);
+  }
 }
 
 function soundAlarm(){
-    var bell = document.getElementById("sound");
-	bell.play();
+  var bell = document.getElementById("sound");
+  bell.play();
 }
