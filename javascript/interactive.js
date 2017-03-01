f8a4763ca8d16dc3ed13fe153c1cf8129cb0cba4
Marc <phyrenight@gmail.com>
2017-03-01 13:59:16 -0500

    undeclared variable in startStop()

--- javascript/interactive.js
+++ javascript/interactive.js
@@ -1,108 +1,121 @@
-var stat = 0; // off setting is zero(0) on setting is one(1)
-var breakOrWork = 0;
+(function(){
+  "use strict";
 
-function increaseBreak() {
-  var breakTime = parseInt(document.getElementById("breaks").innerHTML);
-  if(breakTime < 60) {
-    breakTime++;
-    document.getElementById("breaks").innerHTML = breakTime;
+  class gameState {
+    constructor(){
+      this.tickTock = "";
+    }
   }
-  else{
-	document.getElementById("breaks").innerHTML = breakTime;
-  }
-}
 
-function decreaseBreak() {
-  var breakTime = parseInt(document.getElementById("breaks").innerHTML);
-  if(breakTime > 0) {
-    breakTime--;
-    document.getElementById("breaks").innerHTML = breakTime;
-  }
-  else {
-	document.getElementById("breaks").innerHTML = breakTime;
-  }
-}
+  var clockbutton = document.getElementById("time_left");
 
-function increaseWork() {
-  var workTime = parseInt(document.getElementById("work").innerHTML);
-  if( workTime < 60){
-    workTime++;
-    document.getElementById("work").innerHTML = workTime;
-  }
-  else{
-	document.getElementById("work").innerHTML = workTime;
-  }
-}
+  clockbutton.addEventListener("click", startStop, false);
 
-function decreaseWork() {
-  var workTime = parseInt(document.getElementById("work").innerHTML);
-  if(workTime > 0){
-    workTime--;
-    document.getElementById("work").innerHTML = workTime;
-  }
-  else {
-    document.getElementById("work").innerHTML = workTime;
-  }
-}
+  var stat = 0; // off setting is zero(0) on setting is one(1)
+  var breakOrWork = 0;
 
-function clock(){
-  /*
-    function: used for timer countdown
-  */
-  alarmAndSwitch();
-  var timerTime = document.getElementById("time_left").innerHTML;
-  timerTime = timerTime + ":00";
-  var ss = timerTime.split(":");
-  var d = new Date();
-  d.setHours(0);
-  d.setMinutes(ss[0]);
-  d.setSeconds(ss[1]);
-  var d2 = new Date(d.valueOf() - 1000);
-  var tempDate = d2.toTimeString().split(" ");
-  var tD = tempDate[0].split(":");
-  document.getElementById("time_left").innerHTML = (tD[1]+ ":" +tD[2]);
-} 
-
-function alarmAndSwitch() {
-  /*
-    function: switches timer between the work and the break time  
-  */
-  var timer = document.getElementById("time_left").innerHTML;
-  if(timer < "00:01"){
-    soundAlarm();
-    if(breakOrWork < 1){
-      breakOrWork = 1;
-      breakTime = document.getElementById("breaks").innerHTML;
-      document.getElementById("time_left").innerHTML = breakTime;
+  function increaseBreak() {
+    var breakTime = parseInt(document.getElementById("breaks").innerHTML);
+    if(breakTime < 60) {
+      breakTime++;
+      document.getElementById("breaks").innerHTML = breakTime;
     }
     else{
-      breakOrWork = 0;
-      workTime = document.getElementById("work").innerHTML;
-      document.getElementById("time_left").innerHTML = workTime;
+	    document.getElementById("breaks").innerHTML = breakTime;
     }
   }
-}
 
-
-function startStop(){
-  /*
-    function: starts and stops counter , then resets it to the current work setting.(reword)
-  */
-  //ssvar tickTock  = 0;
-  var workTime = document.getElementById("work").innerHTML;
-  if (stat == 1){ // stops the clock
-	  clearInterval(tickTock);
-    stat = 0;
-	  document.getElementById("time_left").innerHTML = workTime;
+  function decreaseBreak() {
+    var breakTime = parseInt(document.getElementById("breaks").innerHTML);
+    if(breakTime > 0) {
+      breakTime--;
+      document.getElementById("breaks").innerHTML = breakTime;
+    }
+    else {
+	    document.getElementById("breaks").innerHTML = breakTime;
+    }
   }
-  else { // starts the clock
-	  stat = 1;
-	  document.getElementById("time_left").innerHTML = workTime;
-	  tickTock = setInterval(clock,1000);
-  }
-}
 
-function soundAlarm(){
-  var bell = document.getElementById("sound");
-  bell.play();
-}
+  function increaseWork() {
+    var workTime = parseInt(document.getElementById("work").innerHTML);
+    if( workTime < 60){
+      workTime++;
+      document.getElementById("work").innerHTML = workTime;
+    }
+    else{
+	    document.getElementById("work").innerHTML = workTime;
+    }
+  }
+
+  function decreaseWork() {
+    var workTime = parseInt(document.getElementById("work").innerHTML);
+    if(workTime > 0){
+      workTime--;
+      document.getElementById("work").innerHTML = workTime;
+    }
+    else {
+      document.getElementById("work").innerHTML = workTime;
+    }
+  }
+
+  function clock(){
+    /*
+      function: used for timer countdown
+    */
+    alarmAndSwitch();
+    var timerTime = document.getElementById("time_left").innerHTML;
+    timerTime = timerTime + ":00";
+    var ss = timerTime.split(":");
+    var d = new Date();
+    d.setHours(0);
+    d.setMinutes(ss[0]);
+    d.setSeconds(ss[1]);
+    var d2 = new Date(d.valueOf() - 1000);
+    var tempDate = d2.toTimeString().split(" ");
+    var tD = tempDate[0].split(":");
+    document.getElementById("time_left").innerHTML = (tD[1]+ ":" +tD[2]);
+  } 
+
+  function alarmAndSwitch() {
+    /*
+      function: switches timer between the work and the break time  
+    */
+    var timer = document.getElementById("time_left").innerHTML;
+    if(timer < "00:01"){
+      soundAlarm();
+      if(breakOrWork < 1){
+        breakOrWork = 1;
+        breakTime = document.getElementById("breaks").innerHTML;
+        document.getElementById("time_left").innerHTML = breakTime;
+      }
+      else{
+        breakOrWork = 0;
+        workTime = document.getElementById("work").innerHTML;
+        document.getElementById("time_left").innerHTML = workTime;
+      }
+    }
+  }
+
+
+  function startStop(){
+    /*
+      function: starts and stops counter , then resets it to the current work setting.(reword)
+    */
+    var workTime = document.getElementById("work").innerHTML;
+    if (stat == 1){ // stops the clock
+	    clearInterval(gameState.tickTock);
+      stat = 0;
+	    document.getElementById("time_left").innerHTML = workTime;
+    }
+    else { // starts the clock
+	    stat = 1;
+	    document.getElementById("time_left").innerHTML = workTime;
+	    gameState.tickTock = setInterval(clock,1000);
+    }
+  }
+
+  function soundAlarm(){
+    var bell = document.getElementById("sound");
+    bell.play();
+  }
+})();
